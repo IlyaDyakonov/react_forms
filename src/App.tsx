a53630e7402422a1eba63680655b72553340d889
IlyaDyakonov <theApuoX@gmail.com>
2024-07-10 23:23:39 +0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import ConvectorRGB from './components/TaskOne/logic';
+import Forms from './components/TaskTwo/forms';
+import './App.css';
+
+
+function App() {
+
+  return (
+    <>
+      <h1>Задача 1!
+        <ConvectorRGB />
+      </h1>
+      <h1>Задача 2!
+        <Forms />
+      </h1>
+    </>
+  )
+}
+
+export default App
